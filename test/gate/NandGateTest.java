851ec8b504e62f6f8c1f58790c0066f7719289c3
Taylor <tjsg1992@gmail.com>
2015-10-22 21:44:55 -0700

    Update tests for multi-gate NAND and NOR Gates and fix bugs that appear.

--- test/gate/NandGateTest.java
+++ test/gate/NandGateTest.java
@@ -10,19 +10,33 @@ import transistor.Connection;
 public class NandGateTest {
 	private Connection myInputConnectionA;
 	private Connection myInputConnectionB;
+	private Connection myInputConnectionC;
+	private Connection myInputConnectionD;
+	
 	private Connection[] myInputs;
 	private NandGate myTestGate;
+	private NandGate myTestGateManyInputs;
 	
 	@Before
 	public void setUp() throws Exception {
-		 myInputConnectionA = new Connection();
-		 myInputConnectionB = new Connection();
+		myInputConnectionA = new Connection();
+		myInputConnectionB = new Connection();
+		myInputConnectionC = new Connection();
+		myInputConnectionD = new Connection();
 		 
-		 myInputs = new Connection[2];
-		 myInputs[0] = myInputConnectionA;
-		 myInputs[1] = myInputConnectionB;
+		myInputs = new Connection[2];
+		myInputs[0] = myInputConnectionA;
+		myInputs[1] = myInputConnectionB;
 		 
-		 myTestGate = new NandGate(myInputs);
+		myTestGate = new NandGate(myInputs);
+		 
+		myInputs = new Connection[4];
+		myInputs[0] = myInputConnectionA;
+		myInputs[1] = myInputConnectionB;
+		myInputs[2] = myInputConnectionC;
+		myInputs[3] = myInputConnectionD;
+
+		myTestGateManyInputs = new NandGate(myInputs);
 	}
 
 	@Test
@@ -48,6 +62,36 @@ public class NandGateTest {
 		myInputConnectionA.powerOff();
 		myInputConnectionB.powerOn();
 		assertTrue(myTestGate.getOutput().hasPower());
+	}
+	
+	@Test
+	public void manyInputAllOnTest() {
+		myInputConnectionA.powerOn();
+		myInputConnectionB.powerOn();
+		myInputConnectionC.powerOn();
+		myInputConnectionD.powerOn();
+		
+		assertFalse(myTestGateManyInputs.getOutput().hasPower());
+	}
+	
+	@Test
+	public void manyInputHalfOnTest() {
+		myInputConnectionA.powerOn();
+		myInputConnectionB.powerOn();
+		myInputConnectionC.powerOff();
+		myInputConnectionD.powerOff();
+		
+		assertTrue(myTestGateManyInputs.getOutput().hasPower());
+	}
+	
+	@Test
+	public void manyInputAllOffTest() {
+		myInputConnectionA.powerOff();
+		myInputConnectionB.powerOff();
+		myInputConnectionC.powerOff();
+		myInputConnectionD.powerOff();
+		
+		assertTrue(myTestGateManyInputs.getOutput().hasPower());
 	}
 
 }
--- test/gate/NorGateTest.java
+++ test/gate/NorGateTest.java
@@ -10,19 +10,33 @@ import transistor.Connection;
 public class NorGateTest {	
 	private Connection myInputConnectionA;
 	private Connection myInputConnectionB;
+	private Connection myInputConnectionC;
+	private Connection myInputConnectionD;
+	
 	private Connection[] myInputs;
 	private NorGate myTestGate;
+	private NorGate myTestGateManyInputs;
 
 	@Before
 	public void setUp() throws Exception {
 		myInputConnectionA = new Connection();
 		myInputConnectionB = new Connection();
+		myInputConnectionC = new Connection();
+		myInputConnectionD = new Connection();
 		 
 		myInputs = new Connection[2];
 		myInputs[0] = myInputConnectionA;
 		myInputs[1] = myInputConnectionB;
 		 
 		myTestGate = new NorGate(myInputs);
+		
+		myInputs = new Connection[4];
+		myInputs[0] = myInputConnectionA;
+		myInputs[1] = myInputConnectionB;
+		myInputs[2] = myInputConnectionC;
+		myInputs[3] = myInputConnectionD;
+		
+		myTestGateManyInputs = new NorGate(myInputs);
 	}
 
 	@Test
@@ -48,5 +62,35 @@ public class NorGateTest {
 		myInputConnectionA.powerOff();
 		myInputConnectionB.powerOn();
 		assertFalse(myTestGate.getOutput().hasPower());
+	}
+	
+	@Test
+	public void manyInputAllOnTest() {
+		myInputConnectionA.powerOn();
+		myInputConnectionB.powerOn();
+		myInputConnectionC.powerOn();
+		myInputConnectionD.powerOn();
+		
+		assertFalse(myTestGateManyInputs.getOutput().hasPower());
+	}
+	
+	@Test
+	public void manyInputHalfOnTest() {
+		myInputConnectionA.powerOn();
+		myInputConnectionB.powerOn();
+		myInputConnectionC.powerOff();
+		myInputConnectionD.powerOff();
+		
+		assertFalse(myTestGateManyInputs.getOutput().hasPower());
+	}
+	
+	@Test
+	public void manyInputAllOffTest() {
+		myInputConnectionA.powerOff();
+		myInputConnectionB.powerOff();
+		myInputConnectionC.powerOff();
+		myInputConnectionD.powerOff();
+		
+		assertTrue(myTestGateManyInputs.getOutput().hasPower());
 	}
 }
